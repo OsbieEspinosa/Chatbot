e5ba014d4cb39f310c8e1c6722a541f292c99430
OsbieEspinosa <66749220+OsbieEspinosa@users.noreply.github.com>
2020-06-15 10:01:45 -0500

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
-const BOLD360_AI_ACCOUNT = 'myAccount'; //Account name
-const BOLD360_AI_DOMAIN = 'myDomain'; //Domain name
-const BOLD360_AI_KBID = 'myKB'; //Can be the Knowledgebase name or its ID
-const BOLD360_AI_CONFIGID = 'myConfigId'; //Domain API ID
+const BOLD360_AI_ACCOUNT = 'compucom'; //Account name
+const BOLD360_AI_DOMAIN = 'compucom.nanorep.co'; //Domain name
+const BOLD360_AI_KBID = '1110663221'; //Can be the Knowledgebase name or its ID
+const BOLD360_AI_CONFIGID = '1483958521'; //Domain API ID
 const BOLD360_AI_APIKEY = 'myApiKey'; //API Key
 const BOLD360_AI_SUPPORT = 'mySupportSite.com'; //Use your own support center here
