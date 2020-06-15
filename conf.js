9981a682c67a07ffc9628d66e338e5b2fab6954e
OsbieEspinosa <66749220+OsbieEspinosa@users.noreply.github.com>
2020-06-15 09:59:09 -0500

    Add files via upload

---
+++ conf.js
@@ -0,0 +1,6 @@
+const BOLD360_AI_ACCOUNT = 'myAccount'; //Account name
+const BOLD360_AI_DOMAIN = 'myDomain'; //Domain name
+const BOLD360_AI_KBID = 'myKB'; //Can be the Knowledgebase name or its ID
+const BOLD360_AI_CONFIGID = 'myConfigId'; //Domain API ID
+const BOLD360_AI_APIKEY = 'myApiKey'; //API Key
+const BOLD360_AI_SUPPORT = 'mySupportSite.com'; //Use your own support center here
